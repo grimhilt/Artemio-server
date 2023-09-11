dcdf82cade0aec9c6256baa6bc7f06aaf994a708
grimhilt <grimhilt@users.noreply.github.com>
2023-09-11 21:25:55 +0200

    auto generate secret_key

---
+++ src/config/config.py
@@ -0,0 +1,19 @@
+import os
+import random
+import string
+
+SECRET_KEY_FILE = './src/config/SECRET_KEY'
+
+def get_secret_key():
+    if os.path.isfile(SECRET_KEY_FILE):
+        # read the secret key from the file
+        with open(SECRET_KEY_FILE, 'r') as file:
+            secret_key = file.read().strip()
+        return secret_key
+    else:
+        # generate a new secret key
+        secret_key = os.urandom(24)
+        # save it to the file
+        with open(SECRET_KEY_FILE, 'w') as file:
+            file.write(str(secret_key))
+        return secret_key
